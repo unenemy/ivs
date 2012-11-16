00dd93b0d299a992a5e2582c9c70e31b6de6c46d
unenemy <unenemy@unenemy-laptop.(none)>
2012-11-17 00:50:51 +0200

    defaults

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
-    puts params
-    lab = params[:lab]
+    lab = params[:lab] || "lab1"
     @filter = Filter.new(params[:harmonics].to_i || 6)
     self.send(lab) if (1..4).map{|i| ["lab",i].join}.include? lab
   end
